965f1ccdadf25247d9921210c22bdfd615751ce9
Corbies <1557655749@qq.com>
2017-05-30 22:00:12 +0800

    frest commit

---
+++ src/main/java/com/llvision/jpa2/repository1/LoginRepository1.java
@@ -0,0 +1,7 @@
+package com.llvision.jpa2.repository1;
+
+import com.llvision.jpa2.entity1.Login1;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  LoginRepository1 extends JpaRepository<Login1, Long> {
+}
---
+++ src/main/java/com/llvision/jpa2/repository2/LoginRepository2.java
@@ -0,0 +1,8 @@
+package com.llvision.jpa2.repository2;
+
+
+import com.llvision.jpa2.entity2.Login2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface  LoginRepository2 extends JpaRepository<Login2, Long> {
+}
